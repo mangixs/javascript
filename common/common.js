d18ad2ae8bce011246c9c36375ffe0bb36c988fc
managix <fnhdcwy@hotmail.com>
2018-02-04 22:45:52 +0800

    finishing

---
+++ common/common.js
@@ -0,0 +1,673 @@
+ /* 
+ * @desc   为元素添加class
+ * @param  {HTMLElement} ele 
+ * @param  {String} cls 
+ */
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) {
+        ele.className += ' ' + cls;
+    }
+}
+/**
+ * 
+ * @desc 判断两个数组是否相等
+ * @param {Array} arr1 
+ * @param {Array} arr2 
+ * @return {Boolean}
+ */
+function arrayEqual(arr1, arr2) {
+    if (arr1 === arr2) return true;
+    if (arr1.length != arr2.length) return false;
+    for (var i = 0; i < arr1.length; ++i) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+/**
+ * @desc 函数防抖 
+ * 与throttle不同的是，debounce保证一个函数在多少毫秒内不再被触发，只会执行一次，
+ * 要么在第一次调用return的防抖函数时执行，要么在延迟指定毫秒后调用。
+ * @example 适用场景：如在线编辑的自动存储防抖。
+ * @param  {Number}   delay         0或者更大的毫秒数。 对于事件回调，大约100或250毫秒（或更高）的延迟是最有用的。
+ * @param  {Boolean}  atBegin       可选，默认为false。
+ *                                  如果`atBegin`为false或未传入，回调函数则在第一次调用return的防抖函数后延迟指定毫秒调用。
+                                    如果`atBegin`为true，回调函数则在第一次调用return的防抖函数时直接执行
+ * @param  {Function} callback      延迟毫秒后执行的函数。`this`上下文和所有参数都是按原样传递的，
+ *                                  执行去抖动功能时，，调用`callback`。
+ *
+ * @return {Function} 新的防抖函数。
+ */
+function debounce(delay, atBegin, callback) {
+    return callback === undefined ? throttle(delay, atBegin, false) : throttle(delay, callback, atBegin !== false);
+};
+/**
+ * @desc 深拷贝，支持常见类型
+ * @param {Any} values
+ */
+function deepClone(values) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == values || "object" != typeof values) return values;
+
+    // Handle Date
+    if (values instanceof Date) {
+        copy = new Date();
+        copy.setTime(values.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (values instanceof Array) {
+        copy = [];
+        for (var i = 0, len = values.length; i < len; i++) {
+            copy[i] = deepClone(values[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (values instanceof Object) {
+        copy = {};
+        for (var attr in values) {
+            if (values.hasOwnProperty(attr)) copy[attr] = deepClone(values[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy values! Its type isn't supported.");
+}
+/**
+ * 
+ * @desc   现金额转大写
+ * @param  {Number} n 
+ * @return {String}
+ */
+function digitUppercase(n) {
+    var fraction = ['角', '分'];
+    var digit = [
+        '零', '壹', '贰', '叁', '肆',
+        '伍', '陆', '柒', '捌', '玖'
+    ];
+    var unit = [
+        ['元', '万', '亿'],
+        ['', '拾', '佰', '仟']
+    ];
+    var head = n < 0 ? '欠' : '';
+    n = Math.abs(n);
+    var s = '';
+    for (var i = 0; i < fraction.length; i++) {
+        s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
+    }
+    s = s || '整';
+    n = Math.floor(n);
+    for (var i = 0; i < unit[0].length && n > 0; i++) {
+        var p = '';
+        for (var j = 0; j < unit[1].length && n > 0; j++) {
+            p = digit[n % 10] + unit[1][j] + p;
+            n = Math.floor(n / 10);
+        }
+        s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
+    }
+    return head + s.replace(/(零.)*零元/, '元')
+        .replace(/(零.)+/g, '零')
+        .replace(/^整$/, '零元整');
+};
+/**
+ * @desc   格式化${startTime}距现在的已过时间
+ * @param  {Date} startTime 
+ * @return {String}
+ */
+function formatPassTime(startTime) {
+    var currentTime = Date.parse(new Date()),
+        time = currentTime - startTime,
+        day = parseInt(time / (1000 * 60 * 60 * 24)),
+        hour = parseInt(time / (1000 * 60 * 60)),
+        min = parseInt(time / (1000 * 60)),
+        month = parseInt(day / 30),
+        year = parseInt(month / 12);
+    if (year) return year + "年前"
+    if (month) return month + "个月前"
+    if (day) return day + "天前"
+    if (hour) return hour + "小时前"
+    if (min) return min + "分钟前"
+    else return '刚刚'
+}
+/**
+ * 
+ * @desc   格式化现在距${endTime}的剩余时间
+ * @param  {Date} endTime  
+ * @return {String}
+ */
+function formatRemainTime(endTime) {
+    var startDate = new Date(); //开始时间
+    var endDate = new Date(endTime); //结束时间
+    var t = endDate.getTime() - startDate.getTime(); //时间差
+    var d = 0,
+        h = 0,
+        m = 0,
+        s = 0;
+    if (t >= 0) {
+        d = Math.floor(t / 1000 / 3600 / 24);
+        h = Math.floor(t / 1000 / 60 / 60 % 24);
+        m = Math.floor(t / 1000 / 60 % 60);
+        s = Math.floor(t / 1000 % 60);
+    }
+    return d + "天 " + h + "小时 " + m + "分钟 " + s + "秒";
+}
+/**
+ * 
+ * @desc 根据name读取cookie
+ * @param  {String} name 
+ * @return {String}
+ */
+function getCookie(name) {
+    var arr = document.cookie.replace(/\s/g, "").split(';');
+    for (var i = 0; i < arr.length; i++) {
+        var tempArr = arr[i].split('=');
+        if (tempArr[0] == name) {
+            return decodeURIComponent(tempArr[1]);
+        }
+    }
+    return '';
+}
+/**
+ * 
+ * @desc 获取浏览器类型和版本
+ * @return {String} 
+ */
+function getExplore() {
+    var sys = {},
+        ua = navigator.userAgent.toLowerCase(),
+        s;
+    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? sys.ie = s[1]:
+        (s = ua.match(/msie ([\d\.]+)/)) ? sys.ie = s[1] :
+        (s = ua.match(/edge\/([\d\.]+)/)) ? sys.edge = s[1] :
+        (s = ua.match(/firefox\/([\d\.]+)/)) ? sys.firefox = s[1] :
+        (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? sys.opera = s[1] :
+        (s = ua.match(/chrome\/([\d\.]+)/)) ? sys.chrome = s[1] :
+        (s = ua.match(/version\/([\d\.]+).*safari/)) ? sys.safari = s[1] : 0;
+    // 根据关系进行判断
+    if (sys.ie) return ('IE: ' + sys.ie)
+    if (sys.edge) return ('EDGE: ' + sys.edge)
+    if (sys.firefox) return ('Firefox: ' + sys.firefox)
+    if (sys.chrome) return ('Chrome: ' + sys.chrome)
+    if (sys.opera) return ('Opera: ' + sys.opera)
+    if (sys.safari) return ('Safari: ' + sys.safari)
+    return 'Unkonwn'
+}
+/**
+ * 
+ * @desc 获取操作系统类型
+ * @return {String} 
+ */
+function getOS() {
+    var userAgent = 'navigator' in window && 'userAgent' in navigator && navigator.userAgent.toLowerCase() || '';
+    var vendor = 'navigator' in window && 'vendor' in navigator && navigator.vendor.toLowerCase() || '';
+    var appVersion = 'navigator' in window && 'appVersion' in navigator && navigator.appVersion.toLowerCase() || '';
+
+    if (/mac/i.test(appVersion)) return 'MacOSX'
+    if (/win/i.test(appVersion)) return 'windows'
+    if (/linux/i.test(appVersion)) return 'linux'
+    if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent)) 'ios'
+    if (/android/i.test(userAgent)) return 'android'
+    if (/win/i.test(appVersion) && /phone/i.test(userAgent)) return 'windowsPhone'
+}
+/**
+ * 
+ * @desc 获取滚动条距顶部的距离
+ */
+function getScrollTop() {
+    return (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+}
+/**
+ * 
+ * @desc 判断元素是否有某个class
+ * @param {HTMLElement} ele 
+ * @param {String} cls 
+ * @return {Boolean}
+ */
+function hasClass(ele, cls) {
+    return (new RegExp('(\\s|^)' + cls + '(\\s|$)')).test(ele.className);
+}
+/**
+ * 
+ * @desc   判断是否为邮箱地址
+ * @param  {String}  str
+ * @return {Boolean} 
+ */
+function isEmail(str) {
+    return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(str);
+}
+/**
+ * 
+ * @desc   判断`obj`是否为空
+ * @param  {Object} obj
+ * @return {Boolean}
+ */
+function isEmptyObject(obj) {
+    if (!obj || typeof obj !== 'object' || Array.isArray(obj))
+        return false
+    return !Object.keys(obj).length
+}
+/**
+ * 
+ * @desc  判断是否为身份证号
+ * @param  {String|Number} str 
+ * @return {Boolean}
+ */
+function isIdCard(str) {
+    return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(str)
+}
+/**
+ * 
+ * @desc   判断是否为手机号
+ * @param  {String|Number} str 
+ * @return {Boolean} 
+ */
+function isPhoneNum(str) {
+    return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str)
+}
+/**
+ * 
+ * @desc 判断浏览器是否支持webP格式图片
+ * @return {Boolean} 
+ */
+function isSupportWebP() {
+    return !![].map && document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0;
+}
+/**
+ * 
+ * @desc   判断是否为URL地址
+ * @param  {String} str 
+ * @return {Boolean}
+ */
+function isUrl(str) {
+    return /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i.test(str);
+}
+/**
+ * 
+ * @desc  获取一个元素的距离文档(document)的位置，类似jQ中的offset()
+ * @param {HTMLElement} ele 
+ * @returns { {left: number, top: number} }
+ */
+function offset(ele) {
+    var pos = {
+        left: 0,
+        top: 0
+    };
+    while (ele) {
+        pos.left += ele.offsetLeft;
+        pos.top += ele.offsetTop;
+        ele = ele.offsetParent;
+    };
+    return pos;
+}
+/**
+ * 
+ * @desc   url参数转对象
+ * @param  {String} url  default: window.location.href
+ * @return {Object} 
+ */
+function parseQueryString(url) {
+    url = url == null ? window.location.href : url
+    var search = url.substring(url.lastIndexOf('?') + 1)
+    if (!search) {
+        return {}
+    }
+    return JSON.parse('{"' + decodeURIComponent(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+}
+/**
+ * 
+ * @desc   url参数转对象
+ * @param  {String} url  default: window.location.href
+ * @return {Object} 
+ */
+function parseQueryString(url) {
+    url = url == null ? window.location.href : url
+    var search = url.substring(url.lastIndexOf('?') + 1)
+    if (!search) {
+        return {}
+    }
+    return JSON.parse('{"' + decodeURIComponent(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+}
+/**
+ * 
+ * @desc 生成指定范围随机数
+ * @param  {Number} min 
+ * @param  {Number} max 
+ * @return {Number} 
+ */
+function randomNum(min, max) {
+    return Math.floor(min + Math.random() * (max - min));
+}
+/**
+ * 
+ * @desc 为元素移除class
+ * @param {HTMLElement} ele 
+ * @param {String} cls 
+ */
+
+
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
+/**
+ * 
+ * @desc 根据name删除cookie
+ * @param  {String} name 
+ */
+function removeCookie(name) {
+    // 设置已过期，系统会立刻删除cookie
+    setCookie(name, '1', -1);
+}
+var requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+/**
+ * 
+ * @desc  在${duration}时间内，滚动条平滑滚动到${to}指定位置
+ * @param {Number} to 
+ * @param {Number} duration 
+ */
+function scrollTo(to, duration) {
+    if (duration < 0) {
+        setScrollTop(to);
+        return
+    }
+    var diff = to - getScrollTop();
+    if (diff === 0) return
+    var step = diff / duration * 10;
+    requestAnimationFrame(
+        function () {
+            if (Math.abs(step) > Math.abs(diff)) {
+                setScrollTop(getScrollTop() + diff);
+                return;
+            }
+            setScrollTop(getScrollTop() + step);
+            if (diff > 0 && getScrollTop() >= to || diff < 0 && getScrollTop() <= to) {
+                return;
+            }
+            scrollTo(to, duration - 16);
+        });
+}
+/**
+ * 
+ * @desc  设置Cookie
+ * @param {String} name 
+ * @param {String} value 
+ * @param {Number} days 
+ */
+function setCookie(name, value, days) {
+    var date = new Date();
+    date.setDate(date.getDate() + days);
+    document.cookie = name + '=' + value + ';expires=' + date;
+}
+/**
+ * 
+ * @desc 设置滚动条距顶部的距离
+ */
+function setScrollTop(value) {
+    window.scrollTo(0, value);
+    return value;
+}
+/**
+ * 
+ * @desc   对象序列化
+ * @param  {Object} obj 
+ * @return {String}
+ */
+function stringfyQueryString(obj) {
+    if (!obj) return '';
+    var pairs = [];
+
+    for (var key in obj) {
+        var value = obj[key];
+
+        if (value instanceof Array) {
+            for (var i = 0; i < value.length; ++i) {
+                pairs.push(encodeURIComponent(key + '[' + i + ']') + '=' + encodeURIComponent(value[i]));
+            }
+            continue;
+        }
+
+        pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+    }
+
+    return pairs.join('&');
+}
+// 数组对比
+function diff_array(arr1, arr2) {
+    var arr3 = [];
+    for (var i = 0; i < arr1.length; i++) {
+        var flag = true;
+        for (var j = 0; j < arr2.length; j++) {
+            if (arr2[j] == arr1[i]) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            arr3.push(arr1[i]);
+        }
+    }
+    return arr3;
+}
+
+//笛卡儿积组合
+function descartes(list, specData) {
+    var point = {};
+    var result = [];
+    var pIndex = null;
+    var tempCount = 0;
+    var temp = [];
+    for (var index in list) {
+        if (typeof list[index] == 'object') {
+            point[index] = {
+                'parent': pIndex,
+                'count': 0
+            }
+            pIndex = index;
+        }
+    }
+    if (pIndex == null) {
+        return list;
+    }
+    while (true) {
+        for (var index in list) {
+            tempCount = point[index]['count'];
+            temp.push({
+                "id": specData[index].id,
+                "name": specData[index].name,
+                "style": specData[index].style,
+                "color": list[index][tempCount].color,
+                "img": list[index][tempCount].img,
+                "value": list[index][tempCount].value
+            });
+        }
+        result.push(temp);
+        temp = [];
+        while (true) {
+            if (point[index]['count'] + 1 >= list[index].length) {
+                point[index]['count'] = 0;
+                pIndex = point[index]['parent'];
+                if (pIndex == null) {
+                    return result;
+                }
+                index = pIndex;
+            } else {
+                point[index]['count']++;
+                break;
+            }
+        }
+    }
+}
+/**
+ * 替换URL目标参数值
+ * @param  {string} arg 参数名
+ * @param  {string} val 参数值
+ * @param  {string} url 目标地址
+ * @return {string}
+ */
+function replaceParamVal(arg, val, url) {
+    url = url || this.location.href.toString();
+    var pattern = arg + '=([^&]*)';
+    var replaceText = arg + '=' + val;
+    if (url.match(pattern)) {
+        var tmp = '/(' + arg + '=)([^&]*)/gi';
+        tmp = url.replace(eval(tmp), replaceText);
+        return tmp;
+    } else {
+        if (url.match('[\?]')) {
+            return url + '&' + replaceText;
+        } else {
+            return url + '?' + replaceText;
+        }
+    }
+    return url + '\n' + arg + '\n' + val;
+}
+//匹配Email地址
+function isEmail(str){
+    if(!str.match(/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)) return "请输入正确的邮箱地址！";
+}
+
+//只能输入数字[0-9]
+function isDigits(str){
+    if(!str.match(/^\d+$/)) return "请输入数字！";
+}
+
+//匹配mobile
+function isMobile(str){
+    if(!str.match(/^((\(\d{2,3}\))|(\d{3}\-))?((13\d{9})|(15\d{9})|(18\d{9}))$/)) return "请输入正确的手机号码格式！";
+}
+
+//以字母开头
+function isFirstEnglish(str){
+    if(!str.match(/^[a-zA-Z]/)) return "必须以字母为开头！";
+}
+
+//判断是否为合法字符(a-zA-Z0-9-_)字母，数字，下划线
+function isRightfulString(str){
+    if(str==null||str=="") return false;
+    var result=str.match(/^[A-Za-z0-9_-]+$/);
+    if(result==null)return false;
+    return true;
+}
+
+//匹配身份证号码
+function istrCard(str){
+    var aCity = { 11: "北京", 12: "天津", 13: "河北", 14: "山西", 15: "内蒙古", 21: "辽宁", 22: "吉林", 23: "黑龙江 ", 31: "上海", 32: "江苏", 33: "浙江", 34: "安徽", 35: "福建", 36: "江西", 37: "山东", 41: "河南", 42: "湖北 ", 43: "湖南", 44: "广东", 45: "广西", 46: "海南", 50: "重庆", 51: "四川", 52: "贵州", 53: "云南", 54: "西藏 ", 61: "陕西", 62: "甘肃", 63: "青海", 64: "宁夏", 65: "新疆", 71: "台湾", 81: "香港", 82: "澳门", 91: "国外 " }
+
+    var iSum = 0;
+    var info = "";
+    str = str.replace(/x$/i, "a");
+    if (aCity[parseInt(str.substr(0, 2))] == null) return "非法地区";
+    sBirthday = str.substr(6, 4) + "-" + Number(str.substr(10, 2)) + "-" + Number(str.substr(12, 2));
+    var d = new Date(sBirthday.replace(/-/g, "/"))
+    if (sBirthday != (d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate())) return "非法生日";
+    for (var i = 17; i >= 0; i--) iSum += (Math.pow(2, i) % 11) * parseInt(str.charAt(17 - i), 11)
+    if (iSum % 11 != 1) return "非法证号";
+    //str.substr(16, 1) % 2 ? "男" : "女"//获取身份证性别
+}
+
+//字符串长度
+function israngeLength(str,para){
+    var paraName = para.split(",");
+    if(str.length<parseInt(paraName[0])||str.length>parseInt(paraName[1])){
+        return "字符长度必须是"+paraName[0]+"到"+paraName[1]+"之间！";
+    }
+}
+
+//数值范围0-10
+function isnumRange(str,para){
+    var paraName = para.split(",");
+    if(str<paraName[0]||str>paraName[1]){
+        return "","数值范围必须是"+paraName[0]+"到"+paraName[1]+"之间！";
+    }
+}
+
+//URL
+function isUrl(str){
+    if(!str.match(/^http:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\’:+!]*([^<>\"])*$/)){
+        return "请输入正确的url地址,必须以http://开头！"
+    }
+}
+
+//整数
+function isInteger(str){
+    if(!str.match(/^[-\+]?\d+$/)) return "请输入整数！";
+}
+
+//浮点数
+function isDoubleNum(str){
+    if(!str.match(/^[-\+]?\d+(\.\d+)?$/)) return "请输入浮点数！";
+}
+
+//字符串最大长度
+function isMaxLength(str,para){
+    if(str.length>para) return "不能超过"+para+"字符！";
+}
+
+//字符串最小长度
+function isMinLength(str,para){
+    if(str.length<para) return "不能少于"+para+"字符！";
+}
+
+//必须输入英文
+function isEnglish(str){
+    if(!str.match(/^[A-Za-z]+$/)) return "请输入英文，不区分大小写！";
+}
+
+//必须输入中文
+function isChineseChar(str){
+    if(!str.match(/^[\u0391-\uFFE5]+$/)) return "请输入中文！";
+}
+
+//判断是否包含中英文特殊字符，除英文"-_"字符外
+function isSpecialChar(str){
+    var reg = RegExp(/[(\ )(\`)(\~)(\!)(\@)(\#)(\$)(\%)(\^)(\&)(\*)(\()(\))(\+)(\=)(\|)(\{)(\})(\')(\:)(\;)(\')(',)(\[)(\])(\.)(\<)(\>)(\/)(\?)(\~)(\！)(\@)(\#)(\￥)(\%)(\…)(\&)(\*)(\（)(\）)(\—)(\+)(\|)(\{)(\})(\【)(\】)(\‘)(\；)(\：)(\”)(\“)(\’)(\。)(\，)(\、)(\？)]+/);
+    if(reg.test(str)) return "不能输入特殊字符！";
+}
+
+//日期验证 2015-9-6
+function isDate1(str,para){
+    var dateformat = /^[0-9]{4}-[0-1]?[0-9]{1}-[0-3]?[0-9]{1}$/;
+    if(!dateformat.test(str)) return "请输入正确的日期格式！"
+}
+
+//日期验证 2015/9/6
+function isDate2(str,para){
+    var dateformat = /^[0-9]{4}[/][0-1]?[0-9]{1}[/][0-3]?[0-9]{1}$/;
+    if(!dateformat.test(str)) return "请输入正确的日期格式！"
+}
+/**
+ * 解析URL参数
+ * @example ?id=12345&a=b
+ * @return Object {id: 12345, a:'b'}
+ */
+function urlParse() {
+  let search = window.location.search;
+  let obj = {};
+  // 正则
+  let reg = /[?&][^?&]+=[^?&]+]/g;
+  // match方法可以在字符串内检索指定的值，返回一个或多个正则表达式的匹配
+  let arr = search.match(reg); // 返回 ["?id=12345", "&a=b"]
+  if (arr) {
+    arr.forEach((item) => {
+      let tempArr = item.substring(1).split('=');
+      // decodeURIComponent() 函数可对 dencodeURIComponent() 函数编码的 URI 进行解码。
+      let key = decodeURIComponent(tempArr[0]);
+      let value = decodeURIComponent(tempArr[1]);
+
+      obj[key] = value;
+    });
+  }
+  return obj;
+}
