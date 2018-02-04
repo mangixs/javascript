40359676f5049a43c83006559fac0da749867f5b
managix <fnhdcwy@hotmail.com>
2018-02-04 22:36:55 +0800

    url prams

--- common.js
+++ common.js
@@ -647,3 +647,27 @@ function isDate2(str,para){
     var dateformat = /^[0-9]{4}[/][0-1]?[0-9]{1}[/][0-3]?[0-9]{1}$/;
     if(!dateformat.test(str)) return "请输入正确的日期格式！"
 }
+/**
+ * 解析URL参数
+ * @example ?id=12345&a=b
+ * @return Object {id: 12345, a:'b'}
+ */
+function urlParse() {
+  let search = window.location.search;
+  let obj = {};
+  // 正则
+  let reg = /[?&][^?&]+=[^?&]+]/g;
+  // match方法可以在字符串内检索指定的值，返回一个或多个正则表达式的匹配
+  let arr = search.match(reg); // 返回 ["?id=12345", "&a=b"]
+  if (arr) {
+    arr.forEach((item) => {
+      let tempArr = item.substring(1).split('=');
+      // decodeURIComponent() 函数可对 dencodeURIComponent() 函数编码的 URI 进行解码。
+      let key = decodeURIComponent(tempArr[0]);
+      let value = decodeURIComponent(tempArr[1]);
+
+      obj[key] = value;
+    });
+  }
+  return obj;
+}
